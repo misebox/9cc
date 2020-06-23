be8f0c182ef3d9f5f070d73f8de8bdc82ca1bbb5
misebox <misebox@gmail.com>
2020-06-24 01:49:43 +0900

    Created a compiler that outputs an integer

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+assert() {
+  expected="$1"
+  input="$2"
+
+  ./9cc "$input" > tmp.s
+  cc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]
+  then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected ex@ected. bit gpt $actual"
+    exit 1
+  fi
+}
+
+
+assert 0 0
+assert 42 42
+
+echo OK
+
